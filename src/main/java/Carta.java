d9d3c82a329b7e093347ca50d709971600685973
vicent.mateu <vicent.mateu@onlean.es>
2022-05-06 19:16:54 +0200

    classCarta

---
+++ src/main/java/Carta.java
@@ -0,0 +1,11 @@
+
+    public class Carta {
+        private String palo;
+        private int numero;
+
+        public Carta(String palo, int numero){
+            this.palo = palo;
+            this.numero = numero;
+        }
+
+    }
